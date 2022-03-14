4be5b03d81a14456c4441a9ad0b81246dd558b70
Han <101443278+Handra1@users.noreply.github.com>
2022-03-14 12:42:05 +0700

    Add files via upload

---
+++ math.py
@@ -0,0 +1,20 @@
+Han = 5
+Riel = 5
+print(Han + Riel)
+print(Han/Riel)
+
+print(19%2)
+
+x = 20
+y = 11
+remain = x % y
+print(remain)
+
+hasil = 2 ** 3
+print(hasil)
+
+print(round(1.48))
+print(round(1.48,1))
+
+import math
+print(math.pow(2,3))
